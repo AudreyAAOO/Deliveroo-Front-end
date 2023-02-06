db30a5ee3f525241a9a6bf47b9f8cf5e34b2c8f5
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-02-06 23:19:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -10,18 +10,22 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
-    const response = await axios.get("https://site--mydeliveroo--hw4gvwsxlwd5.code.run/"); // http://localhost:3000/ "https://site--mydeliveroo--hw4gvwsxlwd5.code.run/
-    //console.log(response.data);
+    try {
+      const response = await axios.get("https://site--mydeliveroo--hw4gvwsxlwd5.code.run/"); // http://localhost:3000/ "https://site--mydeliveroo--hw4gvwsxlwd5.code.run/
+      //console.log(response.data);
 
-    setData(response.data);
-    setIsLoading(false);
-
+      setData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      //res.status(400).json(error.message);
+    }
   };
 
   useEffect(() => {
     fetchData();
     console.log("useEffect executed");
   }, []);
+
   //! COMPORTEMENTS
 
 
@@ -33,13 +37,56 @@ function App() {
 
     <Header />
     <main>
-      <div>afficher data : </div>
+      <div>
+
+        <div>
+          <h2>{data.restaurant.name}</h2>
+          <p>{data.restaurant.description}</p>
+
+        </div>
+
+        <div className="containerMap">
+
+
+          {data.categories.map((element, i) => {
+            console.log("elem: ", element);
+            console.log("elem.name: ", element.name);
+            console.log("elem.meals: ", element.meals);
+            return (
+              <>
+
+                {/* <div id={element.id} key={i} data={element} name={element.name} >{element}</div> */}
+                <div id={element.id} key={i} data={element} name={element.name}>
+                  <h2>{element.name}</h2>
+                </div>
+                {element.meals.map((element, num) => {
+                  console.log("2. elem: ", element);
+                  console.log("2. elem.meals: ", element.meals);
+                  return (
+                    <section key={num} donnees={element} classname="containerMeal">
+
+                      <h3>{element.title}</h3>
+
+                      <p>{element.description}</p>
+
+                    </section>
+                  );
+                })};
+
+
+
+
+              </>);
+
+          })}
+
+
+        </div>
+
+      </div>
+
 
     </main>
-
-
-
-
 
   </>);
 }
